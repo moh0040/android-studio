d98af59e35ec54b2cf010991d946118efe405019
saeed <sharifi.vsb@gmail.com>
2017-03-25 13:51:52 +0100

    mobile application

---
+++ php/dbConnect.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','mysql.hostinger.in');
+ define('USER','u231442339_user');
+ define('PASS','Madar1131367');
+ define('DB','u231442339_db');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
